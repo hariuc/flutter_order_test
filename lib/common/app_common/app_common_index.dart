c783872885792ee6c6910fdf9cac8a86dbae345e
Piotr Hariuc <acvatoris@gmail.com>
2023-07-28 18:42:41 +0300

    +

--- lib/common/app_common/app_common_index.dart
+++ lib/common/app_common/app_common_index.dart
@@ -6,5 +6,3 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 
 part 'app_utils/app_commons_utils.dart';
-
-part 'app_utils/pop_up_common_utils.dart';
--- lib/order_app.dart
+++ lib/order_app.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_order_test/app/pages/main_page/main_page.dart';
+import 'package:flutter_order_test/common/app_common/app_common_index.dart';
 
 class OrderApp extends StatelessWidget {
   const OrderApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider();
+    return MultiBlocProvider(
+      providers: AppCommonsUtils.getBlocProvidersList(context: context),
+      child: MaterialApp(
+        home: const MainPage(),
+        theme: ThemeData(
+          brightness: Brightness.light,
+          useMaterial3: true,
+        ),
+      ),
+    );
   }
 }
